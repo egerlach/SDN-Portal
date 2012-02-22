660de441a5f2cde4a59e6c521314b1a6beb6db03
Joe Wood <joe@farlizard.com>
2012-02-22 11:38:37 -0500

    Searching by tags is all good now.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
 	
   def index
-  	# if there's a search then it finds newsposts LIKE the search term
+  	# if there's a tag search then the newsposts of the tag are found
+  	if params[:tag]
+  		@newses = Tag.find_by_name(params[:tag]).newsposts.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+  	else # if there's a search then it finds newsposts LIKE the search term
 	@newses = params[:search] ? Newspost.where("title LIKE ?", "%#{params[:search]}%").order("created_at DESC").paginate(:page => params[:page], :per_page => 5) : Newspost.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
-  
+  	end
+
 	respond_to do |format|
       format.html # index.html.erb
 	  format.json
